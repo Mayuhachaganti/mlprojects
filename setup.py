7717e08df7f6d7e878c0d87adfed6578bc7bb72d
Mayuha Chaganti <chagantimayuha23@gmail.com>
2026-03-12 15:39:02 +0530

    setupandRequirement

---
+++ setup.py
@@ -0,0 +1,29 @@
+# setuptools is a builder tool that bundles project for distribution
+# find_packages track entire project structure and should always have init.py
+# setup is a function from setuptools() which contains metadata of the project
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Mayuha',
+    author_email='chagantimayuha23@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
